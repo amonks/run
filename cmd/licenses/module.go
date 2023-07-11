c1b6c27e5e9006dc0b785559f4f73a9d4f763846
monks <a@monks.co>
2023-07-11 03:10:06 -0500

    cmd/licenses: remove package godoc

--- cmd/licenses/module.go
+++ cmd/licenses/module.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package module defines the module.Version type
-// along with support code.
 package main
 
 // IMPORTANT NOTE
