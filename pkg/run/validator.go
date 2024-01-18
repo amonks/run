f225d79718550fb87fe3f56bc5dbbccac00235e6
monks <a@monks.co>
2024-01-17 23:56:26 -0600

    feat: remove groups, allowing short tasks with no cmd
    First, we had "long" and "short" tasks, which both had to have "cmd"
    set. Then, we added "group" tasks, which meant "long task without cmd".
    There was no way to represent "short task without cmd".
    
    Instead, we forgo the "group" type, and allow both "long" and "short"
    tasks to have no "cmd" set.
    
    BREAKING CHANGE: running a taskfile with "group" tasks will now produce
    an error.

--- pkg/run/validator.go
+++ pkg/run/validator.go
@@ -63,30 +63,11 @@ func (v validator) validateTask(ts Tasks, t Task) []error {
 	}
 
 	if meta.Type != "long" && meta.Type != "short" && meta.Type != "group" {
-		problems = append(problems, fmt.Errorf("Task '%s' has invalid type '%s'; must be 'long', 'short', or 'group'.", meta.ID, meta.Type))
+		problems = append(problems, fmt.Errorf("Task '%s' has invalid type '%s'; must be 'long' or 'short'.", meta.ID, meta.Type))
 	}
 
 	if meta.Type == "group" {
-		if len(meta.Dependencies) == 0 {
-			problems = append(problems, fmt.Errorf("Task '%s' is a group, but has no dependencies. Groups must include at least one dependency.", meta.ID))
-		}
-		if len(meta.Triggers) > 0 {
-			problems = append(problems, fmt.Errorf("Task '%s' is a group, but has triggers. Groups may not have triggers.", meta.ID))
-		}
-		if len(meta.Watch) > 0 {
-			problems = append(problems, fmt.Errorf("Task '%s' is a group, but has watch. Groups may not have watch.", meta.ID))
-		}
-		if s, isScript := t.(*scriptTask); isScript {
-			if s.script != "" {
-				problems = append(problems, fmt.Errorf("Task '%s' is a group, but has a cmd. The cmd will not be executed.", meta.ID))
-			}
-		}
-	} else {
-		if s, isScript := t.(*scriptTask); isScript {
-			if s.script == "" {
-				problems = append(problems, fmt.Errorf("Task '%s' is not a group, but has no cmd. It should be a group.", meta.ID))
-			}
-		}
+		problems = append(problems, fmt.Errorf("Task '%s' is a group, but group tasks are deprecated. Mark it as 'long' or 'short'. To preserve its current behavior, use 'long'.", meta.ID))
 	}
 
 	for _, id := range meta.Dependencies {
