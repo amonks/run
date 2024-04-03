4ba8c950782bcbabca6703fc6ae871e9c3ee3146
monks <a@monks.co>
2024-04-03 02:16:22 -0500

    build: produce signed+notarized+universal macos binaries
    closes #108
    
    I tried to do this in goreleaser, but to be honest I find their docs
    maddeningly opaque. I couldn't figure out how to build two macos
    binaries, and then lipo them together and produce a third archive.
    
    So instead, we now build manually with ./publish.bash.

--- tools.go
+++ tools.go
@@ -5,7 +5,6 @@ package run
 
 import (
 	_ "github.com/charmbracelet/vhs"
-	_ "github.com/goreleaser/goreleaser"
 	_ "golang.org/x/tools/cmd/stringer"
 	_ "golang.org/x/vuln/cmd/govulncheck"
 	_ "honnef.co/go/tools/cmd/staticcheck"
